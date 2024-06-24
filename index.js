f5e64d11ad8516b848a1fa6bdcd76e8131427316
Joeken10 <joekentafir@gmail.com>
2024-06-24 16:05:28 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "joeken10";
+
+const welcomeMessage = "Welcome to Flatbook,  ${joeken10}!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting =  "Welcome, ${j.slice(0,1)}!"
+
+
+
